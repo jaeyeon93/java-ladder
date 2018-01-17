c0f5d30da4ac92f5accd2a0eb9e9b7d71889285f
jimmy <foodsksms@gmail.com>
2018-01-17 20:42:52 +0900

    Middle finish

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,9 +1,77 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 class Ladder {
     private static final Logger logger = LoggerFactory.getLogger(Ladder.class);
- public static void main(String [] args){
-     logger.info("hello ladder");
- }
+    ArrayList<String> results = new ArrayList<String>();
+
+    public int peopleInput(String peopleInput){
+        return Integer.parseInt(peopleInput);
+    }
+
+    public int ladderInput(String ladderInput){
+        return Integer.parseInt(ladderInput);
+    }
+
+    public String peoplePrint(int people){
+        String result = "";
+        for(int i = 0; i < (2 * people - 1); i ++){
+            checkOdd(i, result);
+        }
+        return result;
+    }
+
+    public String add(String result){
+        Random rnd = new Random();
+        if (rnd.nextInt(10) >= 6){
+            result += "ㅡ";
+        } else {
+            result += " ";
+        }
+        return result;
+        //logger.info("{}",result);
+    }
+
+    public String checkOdd(int i, String result){
+        if (i % 2 == 0) {
+            result += "ㅣ";
+        } else {
+            add(result);
+        }
+        logger.info("{}",result);
+        return result;
+    }
+
+
+
+    public void process(int people, int height){
+        for (int i = 0; i < height; i++) {
+            results.add(checkOdd(people, peoplePrint(people)));
+        }
+    }
+
+    public void heightPrint(){
+        for (String result:results) {
+            System.out.println(result);
+        }
+    }
+
+    public static void main(String [] args){
+        //input영역
+        Scanner scanner = new Scanner(System.in);
+        Ladder ladder = new Ladder();
+        logger.info("참여할 사람은 몇 명 인가요?");
+        String peopleInput = scanner.nextLine();
+        logger.info("최대 사다리 높이는 몇 개인가요?");
+        String ladderInput = scanner.nextLine();
+        int people = ladder.peopleInput(peopleInput);
+        int ladderHeight = ladder.ladderInput(ladderInput);
+        //output영역
+        ladder.process(people, ladderHeight);
+        //ladder.heightPrint();
+    }
 }
