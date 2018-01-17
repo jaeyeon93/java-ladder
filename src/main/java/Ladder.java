4b597ef29d2874e25507754409b336fab94bfab9
jimmy <foodsksms@gmail.com>
2018-01-17 17:45:57 +0900

    Java ladder step1 start

---
+++ src/main/java/Ladder.java
@@ -0,0 +1,9 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class Ladder {
+    private static final Logger logger = LoggerFactory.getLogger(Ladder.class);
+ public static void main(String [] args){
+     logger.info("hello ladder");
+ }
+}
