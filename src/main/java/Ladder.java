38a3bd4d899c3c8a0f1aaf09bf15f8f69a66ecaf
jimmy <foodsksms@gmail.com>
2018-01-18 12:08:51 +0900

    finish edit step1

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -9,14 +9,6 @@ class Ladder {
     private static final Logger logger = LoggerFactory.getLogger(Ladder.class);
     ArrayList<String> results = new ArrayList<String>();
 
-    public static int peopleInput(String peopleInput){
-        return Integer.parseInt(peopleInput);
-    }
-
-    public static int ladderInput(String ladderInput){
-        return Integer.parseInt(ladderInput);
-    }
-
     public String peoplePrint(int people){
         String result = "";
         for(int i = 0; i < (2 * people) - 1; i++){
@@ -54,21 +46,5 @@ class Ladder {
         for (String result:results) {
             System.out.println(result);
         }
-    }
-
-    public static void main(String [] args){
-        Scanner scanner = new Scanner(System.in);
-        Ladder ladder = new Ladder();
-        LadderInput input = new LadderInput();
-        logger.info("참여할 사람은 몇 명 인가요?");
-        String peopleInput = scanner.nextLine();
-        logger.info("최대 사다리 높이는 몇 개인가요?");
-        String ladderInput = scanner.nextLine();
-        int people = peopleInput(peopleInput);
-        int ladderHeight = ladderInput(ladderInput);
-
-        //output영역
-        ladder.process(people, ladderHeight);
-        ladder.heightPrint();
     }
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -7,17 +7,18 @@ import org.slf4j.LoggerFactory;
 public class LadderTest {
     private static final Logger logger = LoggerFactory.getLogger(LadderTest.class);
     Ladder ladder;
-    Ladder input;
+    // class area
+    LadderInput input;
 
     @Before
     public void setUp() {
-        Ladder ladder = new Ladder();
+        ladder = new Ladder();
         LadderInput input = new LadderInput();
     }
 
     @Test
     public void peopleInput() {
-        int people = input.peopleInput("3");
+        int people = ladder.peopleInput("3");
         assertEquals(3, people);
     }
 
@@ -29,8 +30,8 @@ public class LadderTest {
 
     @Test
     public void addLadder() {
-        String add = ladder.add(8);
-        assertEquals("|", add);
+        String add = ladder.add(6);
+        assertEquals("-", add);
     }
 
     @Test
