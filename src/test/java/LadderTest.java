1418264b0c7afe18ef3debaed20c423f3be06def
jimmy <foodsksms@gmail.com>
2018-01-26 11:26:12 +0900

    Make Test code

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -3,24 +3,25 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class LadderTest {
     private static final Logger logger = LoggerFactory.getLogger(LadderTest.class);
-    ArrayList<Boolean> points = new ArrayList<Boolean>(Arrays.asList(true, true, false, true));
     Ladder ladder;
-    LadderInput input;
-    Line line;
-
+    ArrayList<Line> lines;
     @Before
     public void setUp() {
         Line line = new Line();
         ladder = new Ladder();
-        LadderInput input = new LadderInput();
+        lines = ladder.createLines(5, 3);
     }
 
+    @Test
+    public void createLinesTest(){
+        lines = ladder.createLines(5,3);
+        assertEquals(lines.size(),3);
+    }
+    
     @Test
     public void addLadder() {
         Boolean add = ladder.returnAdd(6);
@@ -32,43 +33,4 @@ public class LadderTest {
         Boolean notAdd = ladder.returnAdd(4);
         assertEquals(false, notAdd);
     }
-
-    @Test
-    public void printLineSize(){
-        int size = points.size();
-        assertEquals(4, size);
-    }
-
-    @Test
-    public void drawLine(){
-        for(int i = 0; i < points.size(); i++){
-            System.out.println(points.get(i));
-        }
-    }
-
-    @Test
-    public void resultTest(){
-        assertEquals(true, points.get(0).booleanValue());
-    }
-
-    @Test
-    public void printBar(){
-        System.out.println(line.result(0));
-    }
-
-
-//    public String result(int i){
-//        checkNext();
-//        if (points.get(i).booleanValue() == true && i != points.size() -1)
-//            return "-----";
-//        return "     ";
-//    }
-//
-//    public String printTrueFalse(int i){
-//        checkNext();
-//        if (points.get(i).booleanValue() == true)
-//            return "true";
-//        return "false";
-//    }
-
 }
